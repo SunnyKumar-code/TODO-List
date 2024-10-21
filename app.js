cda9dd60ad46bec781e6cb203153cf5d9776e141
SunnyKumar-code <sk202542@gmail.com>
2024-10-21 08:43:57 +0530

    make to do list app

---
+++ app.js
@@ -0,0 +1,23 @@
+let index = document.querySelector("#taskInput");
+let addbtn = document.querySelector("#btn");
+let ul = document.querySelector("#taskList");
+
+addbtn.addEventListener("click", function() {
+    let li = document.createElement("li");
+    li.textContent = index.value;
+
+    let delbtn = document.createElement("i");
+    delbtn.classList.add("fa-solid", "fa-xmark", "del-btn");
+    li.appendChild(delbtn);
+    ul.appendChild(li);
+    index.value = "";
+   
+});
+
+ul.addEventListener("click", function(event) {
+    if (event.target.classList.contains("del-btn")) {
+        event.target.parentElement.remove();
+    }
+});
+
+
